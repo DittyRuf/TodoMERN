ab734f20e2a6b2f613e69e5eee0b5dc41ad302f5
Kamil <kam.nas21@wp.pl>
2022-01-24 19:15:43 +0100

    Do checkout bg edit button

--- client/src/components/UI/EditTodo/EditTodo.tsx
+++ client/src/components/UI/EditTodo/EditTodo.tsx
@@ -1,9 +1,24 @@
 import styled from './EditTodo.module.scss';
+import checkIcon from './../../../assets/svg/icon-check.svg';
+import React, { useState } from 'react';
 
 const EditTodo = () => {
+	const [edited, setEdited] = useState('');
+
+	const handleClick = () => {
+		setEdited(edited === '' ? '--complete' : '');
+	};
+
+	const modified = `edit__button${edited}`;
+
 	return (
 		<div className={styled.edit}>
-			<button className={styled.edit__button}>Hi</button>
+			<button
+				className={`${styled.edit__button} ${edited && styled[modified]}`}
+				onClick={handleClick}
+			>
+				<img src={checkIcon} alt='' />
+			</button>
 		</div>
 	);
 };
